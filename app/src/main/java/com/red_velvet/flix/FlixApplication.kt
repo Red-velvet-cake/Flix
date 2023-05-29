045c7aecce56f5ed75d7b49488b1a9a72e21f27d
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-05-29 10:55:05 +0300

    happy initial commit

---
+++ app/src/main/java/com/red_velvet/flix/FlixApplication.kt
@@ -0,0 +1,9 @@
+package com.red_velvet.flix
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FlixApplication: Application(){
+
+}
---
+++ app/src/main/java/com/red_velvet/flix/ui/base/BaseViewModel.kt
@@ -0,0 +1,8 @@
+package com.red_velvet.flix.ui.base
+
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel : ViewModel() {
+
+}
+
