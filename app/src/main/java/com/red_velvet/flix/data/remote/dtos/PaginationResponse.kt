4ec28631996e01ab9cc1b716139accb661eac3ee
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-05-29 12:46:53 +0300

    create common dtos

---
+++ app/src/main/java/com/red_velvet/flix/data/remote/dtos/PaginationResponse.kt
@@ -0,0 +1,14 @@
+package com.red_velvet.flix.data.remote.dtos
+
+import com.google.gson.annotations.SerializedName
+
+data class PaginationResponse<T>(
+    @SerializedName("page")
+    val page: Int?,
+    @SerializedName("results")
+    val items: List<T>?,
+    @SerializedName("total_pages")
+    val totalPages: Int?,
+    @SerializedName("total_results")
+    val totalResults: Int?,
+)
