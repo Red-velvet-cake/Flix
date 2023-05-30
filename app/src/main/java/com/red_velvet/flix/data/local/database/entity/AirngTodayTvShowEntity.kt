4024e01524a94d586744bf4d6f6b875369b0f7ac
jalal cheff <jlalalmyahy571@gmail.com>
2023-05-30 13:40:23 +0300

    indentate classes

--- app/src/main/java/com/red_velvet/flix/data/local/database/entity/AirngTodayTvShowEntity.kt
+++ app/src/main/java/com/red_velvet/flix/data/local/database/entity/AirngTodayTvShowEntity.kt
@@ -2,12 +2,13 @@ package com.red_velvet.flix.data.local.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+
 @Entity
 data class AirngTodayTvShowEntity(
-    @PrimaryKey(autoGenerate = true) val id : Long,
-    val title : String,
-    val originalLanguage : String,
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val title: String,
+    val originalLanguage: String,
     val overview: String,
-    val imageUrl : String,
-    val date : String
+    val imageUrl: String,
+    val date: String,
 )
--- app/src/main/java/com/red_velvet/flix/data/local/database/entity/NowPlayingMovieEntity.kt
+++ app/src/main/java/com/red_velvet/flix/data/local/database/entity/NowPlayingMovieEntity.kt
@@ -2,12 +2,13 @@ package com.red_velvet.flix.data.local.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+
 @Entity
-data class NowPlayingMovieEntity (
-    @PrimaryKey(autoGenerate = true) val id : Long,
-    val title : String,
-    val originalLanguage : String,
+data class NowPlayingMovieEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val title: String,
+    val originalLanguage: String,
     val overview: String,
-    val imageUrl : String,
-    val date : String
-        )
+    val imageUrl: String,
+    val date: String,
+)
