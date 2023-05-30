5c9be1ea55edf6851be6934560b7f1e8f652c2a8
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-05-30 13:00:42 +0300

    create network module

--- app/src/main/java/com/red_velvet/flix/di/NetworkModule.kt
+++ app/src/main/java/com/red_velvet/flix/di/NetworkModule.kt
@@ -1,11 +1,57 @@
 package com.red_velvet.flix.di
 
+import com.red_velvet.flix.data.remote.AuthorizationInterceptor
+import com.red_velvet.flix.data.remote.MoviesService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideMovieService(retrofit: Retrofit): MoviesService {
+        return retrofit.create(MoviesService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        client: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .client(client)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(authInterceptor: AuthorizationInterceptor): OkHttpClient {
+        return OkHttpClient().newBuilder()
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideAuthInterceptor(): AuthorizationInterceptor {
+        return AuthorizationInterceptor()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGsonFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
 
 }
