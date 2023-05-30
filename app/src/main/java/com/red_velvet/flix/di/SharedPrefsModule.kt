499b232d3de5979185170970ef79bf77db9bb57a
jalal cheff <jlalalmyahy571@gmail.com>
2023-05-30 12:55:41 +0300

    add single ton annotation

--- app/src/main/java/com/red_velvet/flix/di/SharedPrefsModule.kt
+++ app/src/main/java/com/red_velvet/flix/di/SharedPrefsModule.kt
@@ -8,16 +8,19 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object SharedPrefsModule {
     @Provides
+    @Singleton
     fun getSharedPrefsUtil(@ApplicationContext context: Context): SharedPreferences {
         return context.getSharedPreferences("sharedPrefsName", Context.MODE_PRIVATE)
     }
 
     @Provides
+    @Singleton
     fun getSharedPrefs(sharedPrefs: SharedPreferences): SharedPrefs {
         return SharedPrefs(sharedPrefs)
     }
