49c57793300eab5cfd4fb07fae30cd8b11c6e132
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-05-30 12:59:33 +0300

    enhance dtos

--- app/src/main/java/com/red_velvet/flix/data/remote/dtos/list/CreateListRequest.kt
+++ app/src/main/java/com/red_velvet/flix/data/remote/dtos/list/CreateListRequest.kt
@@ -4,10 +4,10 @@ package com.red_velvet.flix.data.remote.dtos.list
 import com.google.gson.annotations.SerializedName
 
 data class CreateListRequest(
+    @SerializedName("name")
+    val name: String?,
     @SerializedName("description")
     val description: String?,
     @SerializedName("language")
-    val language: String?,
-    @SerializedName("name")
-    val name: String?
+    val language: String?
 )
