f054cdfc3c0a52901e1c48603435aeda334483c2
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-05-31 19:04:48 +0300

    complete mapping movie dto to entites or models

--- app/src/main/java/com/red_velvet/flix/domain/mapper/MovieDtoMapper.kt
+++ app/src/main/java/com/red_velvet/flix/domain/mapper/MovieDtoMapper.kt
@@ -1,6 +1,9 @@
 package com.red_velvet.flix.domain.mapper
 
 import com.red_velvet.flix.BuildConfig
+import com.red_velvet.flix.data.local.database.entity.PopularMovieEntity
+import com.red_velvet.flix.data.local.database.entity.TopRatedMovieEntity
+import com.red_velvet.flix.data.local.database.entity.UpcomingMovieEntity
 import com.red_velvet.flix.data.remote.dtos.movie.MovieDto
 import com.red_velvet.flix.domain.model.Movie
 
@@ -15,28 +18,53 @@ fun MovieDto.toMovie(): Movie {
     )
 }
 
-/*
-* TODO: Taha Please Implement the following functions
-* */
+fun List<MovieDto>.toMovieList(): List<Movie> {
+    return map { it.toMovie() }
+}
 
-//fun MovieDto.toPopularMovieEntity(): PopularMovieEntity {}
-//
-//fun MovieDto.toUpcomingMovieEntity(): UpcomingMovieEntity {}
-//
-//fun MovieDto.toTopRatedMovieEntity(): TopRatedMovieEntity {}
-//
-//fun List<MovieDto>.toMovieList(): List<Movie> {
-//    return map { it.toMovie() }
-//}
-//
-//fun List<MovieDto>.toPopularMovieEntityList(): List<PopularMovieEntity> {
-//    return map { it.toPopularMovieEntity() }
-//}
-//
-//fun List<MovieDto>.toUpcomingMovieEntityList(): List<UpcomingMovieEntity> {
-//    return map { it.toUpcomingMovieEntity() }
-//}
-//
-//fun List<MovieDto>.toTopRatedMovieEntityList(): List<TopRatedMovieEntity> {
-//    return map { it.toTopRatedMovieEntity() }
-//}
+
+fun MovieDto.toPopularMovieEntity(): PopularMovieEntity {
+    return PopularMovieEntity(
+        id = (id ?: 0).toLong(),
+        title = title ?: "",
+        originalLanguage = originalLanguage ?: "",
+        overview = overview ?: "",
+        imageUrl = BuildConfig.IMAGE_BASE_PATH + posterPath,
+        date = releaseDate ?: ""
+    )
+}
+
+fun List<MovieDto>.toPopularMovieEntityList(): List<PopularMovieEntity> {
+    return map { it.toPopularMovieEntity() }
+}
+
+
+fun MovieDto.toUpcomingMovieEntity(): UpcomingMovieEntity {
+    return UpcomingMovieEntity(
+        id = (id ?: 0).toLong(),
+        title = title ?: "",
+        originalLanguage = originalLanguage ?: "",
+        overview = overview ?: "",
+        imageUrl = BuildConfig.IMAGE_BASE_PATH + posterPath,
+        date = releaseDate ?: ""
+    )
+}
+
+fun List<MovieDto>.toUpcomingMovieEntityList(): List<UpcomingMovieEntity> {
+    return map { it.toUpcomingMovieEntity() }
+}
+
+fun MovieDto.toTopRatedMovieEntity(): TopRatedMovieEntity {
+    return TopRatedMovieEntity(
+        id = (id ?: 0).toLong(),
+        title = title ?: "",
+        originalLanguage = originalLanguage ?: "",
+        overview = overview ?: "",
+        imageUrl = BuildConfig.IMAGE_BASE_PATH + posterPath,
+        date = releaseDate ?: ""
+    )
+}
+
+fun List<MovieDto>.toTopRatedMovieEntityList(): List<TopRatedMovieEntity> {
+    return map { it.toTopRatedMovieEntity() }
+}
