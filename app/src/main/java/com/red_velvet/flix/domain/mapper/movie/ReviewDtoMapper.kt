aafab82634bc837fa6b377f14ae45e5108b25922
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-05-31 20:16:25 +0300

    complete review dto mapper

---
+++ app/src/main/java/com/red_velvet/flix/domain/mapper/movie/ReviewDtoMapper.kt
@@ -0,0 +1,30 @@
+package com.red_velvet.flix.domain.mapper.movie
+
+import com.red_velvet.flix.data.remote.dtos.review.AuthorDetailsDto
+import com.red_velvet.flix.data.remote.dtos.review.ReviewDto
+import com.red_velvet.flix.domain.model.movie.AuthorDetails
+import com.red_velvet.flix.domain.model.movie.Review
+import com.red_velvet.flix.domain.utils.orZero
+
+
+fun ReviewDto.toReview(): Review {
+    return Review(
+        id = id.orEmpty(),
+        author = authorDetails?.toAuthorDetails()!!,
+        content = content.orEmpty(),
+        createdAt = createdAt.orEmpty(),
+        updatedAt = updatedAt.orEmpty(),
+        imageUrl = authorDetails.avatarPath.orEmpty(),
+        rating = authorDetails.rating.orZero(),
+        username = authorDetails.username.orEmpty(),
+    )
+}
+
+fun AuthorDetailsDto.toAuthorDetails(): AuthorDetails {
+    return AuthorDetails(
+        avatarPath = avatarPath.orEmpty(),
+        name = name.orEmpty(),
+        rating = rating.orZero(),
+        username = username.orEmpty(),
+    )
+}
