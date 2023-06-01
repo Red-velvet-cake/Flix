7625931046adab8daa70790dd7cb02b893e72f61
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-01 21:15:16 +0300

    complete AccountDto mapper

--- app/src/main/java/com/red_velvet/flix/domain/mapper/account/AccountDtoMapper.kt
+++ app/src/main/java/com/red_velvet/flix/domain/mapper/account/AccountDtoMapper.kt
@@ -1,2 +1,15 @@
 package com.red_velvet.flix.domain.mapper.account
 
+import com.red_velvet.flix.BuildConfig
+import com.red_velvet.flix.data.remote.dtos.account.AccountDto
+import com.red_velvet.flix.domain.model.account.Account
+import com.red_velvet.flix.domain.utils.orZero
+
+fun AccountDto.toModel(): Account {
+    return Account(
+        id = id.orZero(),
+        name = name.orEmpty(),
+        username = username.orEmpty(),
+        avatarPath = BuildConfig.IMAGE_BASE_PATH + avatar?.avatarPath,
+    )
+}
