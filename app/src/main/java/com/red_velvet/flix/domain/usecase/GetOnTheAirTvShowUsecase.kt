dddcd2e95bc0322f595f0ce48bd0d67aabf2c345
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-01 23:28:21 +0300

    add usecases classes

---
+++ app/src/main/java/com/red_velvet/flix/domain/usecase/GetOnTheAirTvShowUsecase.kt
@@ -0,0 +1,16 @@
+package com.red_velvet.flix.domain.usecase
+
+import com.red_velvet.flix.data.repository.TVShowsRepository
+import com.red_velvet.flix.domain.mapper.series.toOnTheAirTvShowModelList
+import com.red_velvet.flix.domain.model.series.TVShow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetOnTheAirTvShowUsecase @Inject constructor(private val tvShowsRepository: TVShowsRepository) {
+    suspend fun getOnTheAirTVShow(): Flow<List<TVShow>> {
+        return tvShowsRepository.getOnTheAirTvShow().map {
+            it.toOnTheAirTvShowModelList()
+        }
+    }
+}
---
+++ app/src/main/java/com/red_velvet/flix/domain/usecase/GetTopRatedTvShowUsecase.kt
@@ -0,0 +1,16 @@
+package com.red_velvet.flix.domain.usecase
+
+import com.red_velvet.flix.data.repository.TVShowsRepository
+import com.red_velvet.flix.domain.mapper.series.toTopRatedTvShowModelList
+import com.red_velvet.flix.domain.model.series.TVShow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetTopRatedTvShowUsecase @Inject constructor(private val tvShowsRepository: TVShowsRepository) {
+    suspend fun getTopRatedTvShow(): Flow<List<TVShow>> {
+        return tvShowsRepository.getTopRatedTvShow().map {
+            it.toTopRatedTvShowModelList()
+        }
+    }
+}
