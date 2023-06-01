6dd8c96968fbd804233e064d63b6294d5adb7aac
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-01 21:47:01 +0300

    complete person dto mapper

--- app/src/main/java/com/red_velvet/flix/domain/model/account/Person.kt
+++ app/src/main/java/com/red_velvet/flix/domain/model/account/Person.kt
@@ -4,5 +4,9 @@ package com.red_velvet.flix.domain.model.account
 data class Person(
     val id: Int,
     val name: String,
-    val imageUrl: String
+    val imageUrl: String,
+    val biography: String,
+    val birthday: String,
+    val knownForDepartment: String,
+    val placeOfBirth: String,
 )
