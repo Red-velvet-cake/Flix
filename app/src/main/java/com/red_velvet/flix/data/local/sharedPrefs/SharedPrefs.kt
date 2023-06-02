2d85294f879f44de183c49d7a35dc57919c5c17f
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2023-06-02 11:50:19 +0300

    add clear session id function

--- app/src/main/java/com/red_velvet/flix/data/local/sharedPrefs/SharedPrefs.kt
+++ app/src/main/java/com/red_velvet/flix/data/local/sharedPrefs/SharedPrefs.kt
@@ -7,21 +7,26 @@ import javax.inject.Inject
 
 class SharedPrefs @Inject constructor(private val sharedPreferences: SharedPreferences) {
 
-    suspend fun setToken( value : String)
-    {
-        withContext(Dispatchers.IO){ sharedPreferences.edit().putString("TOKEN" , value).apply()}
+    suspend fun setToken(value: String) {
+        withContext(Dispatchers.IO) { sharedPreferences.edit().putString("TOKEN", value).apply() }
     }
-    suspend fun getToken() : String?
-    {
-        return withContext(Dispatchers.IO){ sharedPreferences.getString("TOKEN" , null)}
+
+    suspend fun getToken(): String? {
+        return withContext(Dispatchers.IO) { sharedPreferences.getString("TOKEN", null) }
     }
-    suspend fun setSessionId(value : String)
-    {
-        withContext(Dispatchers.IO){ sharedPreferences.edit().putString("SESSION_ID" , value).apply()}
+
+    suspend fun setSessionId(value: String) {
+        withContext(Dispatchers.IO) {
+            sharedPreferences.edit().putString("SESSION_ID", value).apply()
+        }
     }
-    suspend fun getSessionId() : String?
-    {
-        return withContext(Dispatchers.IO){ sharedPreferences.getString("SESSION_ID" , null)}
+
+    suspend fun getSessionId(): String? {
+        return withContext(Dispatchers.IO) { sharedPreferences.getString("SESSION_ID", null) }
+    }
+
+    suspend fun clearSessionId() {
+        sharedPreferences.edit().clear().apply()
     }
 
 }
