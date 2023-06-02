032e336e3980160d6c7d0378f46d862c9350914c
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2023-06-02 09:44:03 +0300

    create user repository interface class and add function

---
+++ app/src/main/java/com/red_velvet/flix/data/repository/UserRepository.kt
@@ -0,0 +1,14 @@
+package com.red_velvet.flix.data.repository
+
+import com.red_velvet.flix.data.remote.dtos.account.AccountDto
+
+interface UserRepository {
+
+    suspend fun login(userName: String, password: String): Boolean?
+
+    suspend fun logout()
+
+    suspend fun getAccountDetails(): AccountDto?
+
+    suspend fun getSessionId(): String?
+}
