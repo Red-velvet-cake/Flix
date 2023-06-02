0d934b9f8a42bf9059519dcb1e9202ea8b1d7fb6
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2023-06-02 17:56:58 +0300

    create login user account use case

---
+++ app/src/main/java/com/red_velvet/flix/domain/usecase/LoginUseCase.kt
@@ -0,0 +1,12 @@
+package com.red_velvet.flix.domain.usecase
+
+import com.red_velvet.flix.data.repository.UserRepository
+import javax.inject.Inject
+
+class LoginUseCase @Inject constructor(
+    private val userRepository: UserRepository
+) {
+    suspend operator fun invoke(userName: String, password: String) {
+        return userRepository.login(userName, password)
+    }
+}
