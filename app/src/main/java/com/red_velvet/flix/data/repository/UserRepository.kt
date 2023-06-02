daa26ceaf6e0bd7afaecf7ef324b6d808f4236e6
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2023-06-02 11:50:51 +0300

    fix login function in user repository interface

--- app/src/main/java/com/red_velvet/flix/data/repository/UserRepository.kt
+++ app/src/main/java/com/red_velvet/flix/data/repository/UserRepository.kt
@@ -4,7 +4,7 @@ import com.red_velvet.flix.data.remote.dtos.account.AccountDto
 
 interface UserRepository {
 
-    suspend fun login(userName: String, password: String): Boolean?
+    suspend fun login(userName: String, password: String)
 
     suspend fun logout()
 
