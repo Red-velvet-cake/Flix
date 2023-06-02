032254752d2e144ba40f5883b142868de1850e9e
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-02 09:04:36 +0300

    add wrap function and implement it to all requests in repository

--- app/src/main/java/com/red_velvet/flix/data/repository/TVShowsRepositoryImpl.kt
+++ app/src/main/java/com/red_velvet/flix/data/repository/TVShowsRepositoryImpl.kt
@@ -44,111 +44,78 @@ class TVShowsRepositoryImpl @Inject constructor(
         }
     }
 
-    private suspend fun <T> wrap(request: suspend () -> Response<T>) {
+    private suspend fun <T> wrap(request: suspend () -> Response<T>): T {
         val response = request()
         if (response.isSuccessful) {
-            val items = response.body()
+            return response.body()!!
         } else {
             throw Throwable()
         }
     }
 
     override suspend fun getTVShowRecommendations(seriesId: Int, page: Int): PaginationDto<TVShowDto> {
-        val response = apiService.getTVShowRecommendations(seriesId, page)
-        if (response.isSuccessful) {
-            return response.body()!!
-        } else {
-            throw Exception(response.message())
+        return wrap {
+            apiService.getTVShowRecommendations(seriesId, page)
         }
     }
 
     override suspend fun getLatestTVShow(): TVShowDto {
-        val response = apiService.getLatestTVShow()
-        if (response.isSuccessful) {
-            return response.body()!!
-        } else {
-            throw Exception(response.message())
+        return wrap {
+            apiService.getLatestTVShow()
         }
     }
 
     override suspend fun getTVShowKeywords(seriesId: Int): KeywordsDto {
-        val response = apiService.getTVShowKeywords(seriesId)
-        if (response.isSuccessful) {
-            return response.body()!!
-        } else {
-            throw Exception(response.message())
+        return wrap {
+            apiService.getTVShowKeywords(seriesId)
         }
     }
 
     override suspend fun getTVShowReviews(seriesId: Int, page: Int): PaginationDto<ReviewDto> {
-        val response = apiService.getTVShowReviews(seriesId, page)
-        if (response.isSuccessful) {
-            return response.body()!!
-        } else {
-            throw Exception(response.message())
+        return wrap {
+            apiService.getTVShowReviews(seriesId, page)
         }
     }
 
     override suspend fun rateTVShow(seriesId: Int, rating: Double): ApiResponse {
-        val response = apiService.rateTVShow(seriesId, rating)
-        if (response.isSuccessful) {
-            return response.body()!!
-        } else {
-            throw Exception(response.message())
+        return wrap {
+            apiService.rateTVShow(seriesId, rating)
         }
     }
 
     override suspend fun getSeasonDetails(seriesId: Int, seasonNumber: Int): SeasonDto {
-        val response = apiService.getSeasonDetails(seriesId, seasonNumber)
-        if (response.isSuccessful) {
-            return response.body()!!
-        } else {
-            throw Exception(response.message())
+        return wrap {
+            apiService.getSeasonDetails(seriesId, seasonNumber)
         }
     }
 
     override suspend fun getSeasonImages(seriesId: Int, seasonNumber: Int): ImagesDto {
-        val response = apiService.getSeasonImages(seriesId, seasonNumber)
-        if (response.isSuccessful) {
-            return response.body()!!
-        } else {
-            throw Exception(response.message())
+        return wrap {
+            apiService.getSeasonImages(seriesId, seasonNumber)
         }
     }
 
     override suspend fun getTVShowVideos(seriesId: Int): TrailersDto {
-        val response = apiService.getTVShowVideos(seriesId)
-        if (response.isSuccessful) {
-            return response.body()!!
-        } else {
-            throw Exception(response.message())
+        return wrap {
+            apiService.getTVShowVideos(seriesId)
         }
     }
 
     override suspend fun getEpisodeDetails(seriesId: Int, seasonNumber: Int, episodeNumber: Int): EpisodeDto {
-        val response = apiService.getEpisodeDetails(seriesId, seasonNumber, episodeNumber)
-        if (response.isSuccessful) {
-            return response.body()!!
-        } else {
-            throw Exception(response.message())
+        return wrap {
+            apiService.getEpisodeDetails(seriesId, seasonNumber, episodeNumber)
         }
     }
 
     override suspend fun getEpisodeImages(seriesId: Int, seasonNumber: Int, episodeNumber: Int): ImagesDto {
-        val response = apiService.getEpisodeImages(seriesId, seasonNumber, episodeNumber)
-        if (response.isSuccessful) {
-            return response.body()!!
-        } else {
-            throw Exception(response.message())
+        return wrap {
+            apiService.getEpisodeImages(seriesId, seasonNumber, episodeNumber)
         }
     }
 
     override suspend fun getEpisodeVideos(seriesId: Int, seasonNumber: Int, episodeNumber: Int): TrailersDto {
-        val response = apiService.getEpisodeVideos(seriesId, seasonNumber, episodeNumber)
-        if (response.isSuccessful) {
-            return response.body()!!
-        } else {
-            throw Exception(response.message())
+        return wrap {
+            apiService.getEpisodeVideos(seriesId, seasonNumber, episodeNumber)
         }
     }
 
@@ -158,12 +125,8 @@ class TVShowsRepositoryImpl @Inject constructor(
         episodeNumber: Int,
         rating: Double
     ): ApiResponse {
-        val response = apiService.rateEpisode(seriesId, seasonNumber, episodeNumber, rating)
-        if (response.isSuccessful) {
-            return response.body()!!
-        } else {
-            throw Exception(response.message())
+        return wrap {
+            apiService.rateEpisode(seriesId, seasonNumber, episodeNumber, rating)
         }
     }
-
 }
