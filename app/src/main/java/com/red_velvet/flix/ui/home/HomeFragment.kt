a61c3e7b2fdc81c420742565de38f629bef77213
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-03 15:41:09 +0300

    create home fragment

---
+++ app/src/main/java/com/red_velvet/flix/ui/home/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.red_velvet.flix.ui.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.red_velvet.flix.R
+import com.red_velvet.flix.databinding.FragmentHomeBinding
+import com.red_velvet.flix.ui.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
+    override val layoutIdFragment = R.layout.fragment_home
+    override val viewModel: HomeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setTitle(true, getString(R.string.app_name))
+    }
+}
