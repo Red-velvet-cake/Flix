fe912b9f729828b4a862672efc7fa07d1cd6b3e1
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-06-03 21:00:30 +0300

    provide other repositories

--- app/src/main/java/com/red_velvet/flix/di/RepositoryModule.kt
+++ app/src/main/java/com/red_velvet/flix/di/RepositoryModule.kt
@@ -1,9 +1,15 @@
 package com.red_velvet.flix.di
 
 import com.red_velvet.flix.data.local.database.dao.MovieDao
+import com.red_velvet.flix.data.local.database.dao.TvShowDao
+import com.red_velvet.flix.data.local.sharedPrefs.SharedPrefs
 import com.red_velvet.flix.data.remote.MoviesService
 import com.red_velvet.flix.data.repository.MovieRepository
 import com.red_velvet.flix.data.repository.MovieRepositoryImp
+import com.red_velvet.flix.data.repository.TVShowsRepository
+import com.red_velvet.flix.data.repository.TVShowsRepositoryImpl
+import com.red_velvet.flix.data.repository.UserRepository
+import com.red_velvet.flix.data.repository.UserRepositoryImp
 import com.red_velvet.flix.domain.utils.ExceptionHandler
 import dagger.Module
 import dagger.Provides
@@ -23,5 +29,25 @@ object RepositoryModule {
         exceptionHandler: ExceptionHandler
     ): MovieRepository {
         return MovieRepositoryImp(moviesService, movieDao, exceptionHandler)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTVShowRepository(
+        moviesService: MoviesService,
+        tvShowDao: TvShowDao,
+        exceptionHandler: ExceptionHandler
+    ): TVShowsRepository {
+        return TVShowsRepositoryImpl(moviesService, tvShowDao, exceptionHandler)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserRepository(
+        moviesService: MoviesService,
+        sharedPref: SharedPrefs,
+        exceptionHandler: ExceptionHandler,
+    ): UserRepository {
+        return UserRepositoryImp(moviesService, sharedPref, exceptionHandler)
     }
 }
