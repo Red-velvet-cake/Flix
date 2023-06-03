61f16bb50877ddf463f741965d3867122510596d
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-06-03 19:37:06 +0300

    create database module

--- app/src/main/java/com/red_velvet/flix/di/DatabaseModule.kt
+++ app/src/main/java/com/red_velvet/flix/di/DatabaseModule.kt
@@ -1,11 +1,48 @@
 package com.red_velvet.flix.di
 
+import android.content.Context
+import androidx.room.Room
+import com.red_velvet.flix.data.local.database.MovieDatabase
+import com.red_velvet.flix.data.local.database.dao.MovieDao
+import com.red_velvet.flix.data.remote.MoviesService
+import com.red_velvet.flix.data.repository.MovieRepository
+import com.red_velvet.flix.data.repository.MovieRepositoryImp
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
 
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context): MovieDatabase {
+        return Room.databaseBuilder(
+            context,
+            MovieDatabase::class.java,
+            "movie_database"
+        ).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMovieDao(movieDatabase: MovieDatabase) = movieDatabase.movieDao()
+
+    @Singleton
+    @Provides
+    fun provideTvShowDao(movieDatabase: MovieDatabase) = movieDatabase.tvShowDao()
+
+    @Singleton
+    @Provides
+    fun provideUserDataDao(movieDatabase: MovieDatabase) = movieDatabase.userDataDao()
+
+    @Singleton
+    @Provides
+    fun provideMovieRepository(service: MoviesService, moviesDao: MovieDao): MovieRepository {
+        return MovieRepositoryImp(service, moviesDao)
+    }
 }
