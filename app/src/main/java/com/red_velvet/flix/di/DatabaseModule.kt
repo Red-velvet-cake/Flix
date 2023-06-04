6f4829c5e86e0d36e65930f0940ea194e180394b
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-04 12:56:59 +0300

    inject database and usecases to hilt

--- app/src/main/java/com/red_velvet/flix/di/DatabaseModule.kt
+++ app/src/main/java/com/red_velvet/flix/di/DatabaseModule.kt
@@ -1,11 +1,29 @@
 package com.red_velvet.flix.di
 
+import android.content.Context
+import androidx.room.Room
+import com.red_velvet.flix.data.local.database.MovieDatabase
+import com.red_velvet.flix.data.local.database.entity.PopularMovieEntity
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideRoom(@ApplicationContext context: Context) = Room.databaseBuilder(
+        context, MovieDatabase::class.java, "movie_database"
+    )
+        .allowMainThreadQueries()
+        .fallbackToDestructiveMigration()
+        .build()
 
+    @Provides
+    @Singleton
+    fun provideMovieDao(db: MovieDatabase) = db.movieDao()
 }
