f1d0b7518cbbf4db14c6a2ede4a61e7a54e77a63
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2023-06-04 15:56:45 +0300

    create search fragment and not complete it

---
+++ app/src/main/java/com/red_velvet/flix/ui/search/SearchFragment.kt
@@ -0,0 +1,15 @@
+import com.red_velvet.flix.ui.search.SearchViewModel
+
+import androidx.fragment.app.viewModels
+import com.red_velvet.flix.R
+import com.red_velvet.flix.databinding.FragmentSearchBinding
+import com.red_velvet.flix.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchFragment: BaseFragment<FragmentSearchBinding, SearchViewModel>() {
+    override val layoutIdFragment: Int = R.layout.fragment_search
+    override val viewModel: SearchViewModel by viewModels()
+
+    private val mediaSearchAdapter by lazy {  }
+}
