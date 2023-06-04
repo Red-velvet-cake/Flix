9c0de29f5d1ceb7958bd8b5868b1a28cf31eb467
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2023-06-04 15:54:45 +0300

    create media search adapter

---
+++ app/src/main/java/com/red_velvet/flix/ui/search/adapter/MediaSearchAdapter.kt
@@ -0,0 +1,16 @@
+package com.red_velvet.flix.ui.search.adapter
+
+import com.red_velvet.flix.R
+import com.red_velvet.flix.ui.base.BaseAdapter
+import com.red_velvet.flix.ui.base.BaseInteractionListener
+import com.red_velvet.flix.ui.search.mediaSearchUiState.MediaUiState
+
+class MediaSearchAdapter(items: List<MediaUiState>, listener: MediaSearchInteractionListener) :
+    BaseAdapter<MediaUiState>(items, listener) {
+    override val layoutId: Int = R.layout.item_search
+
+}
+
+interface MediaSearchInteractionListener : BaseInteractionListener {
+    fun onClickMediaResult(media: MediaUiState)
+}
