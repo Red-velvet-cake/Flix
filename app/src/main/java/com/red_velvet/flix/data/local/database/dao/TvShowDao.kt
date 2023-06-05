b45a9b9da357296758bbdcee80a33df8b169a2e0
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2023-06-05 18:49:07 +0300

    change return types

--- app/src/main/java/com/red_velvet/flix/data/local/database/dao/TvShowDao.kt
+++ app/src/main/java/com/red_velvet/flix/data/local/database/dao/TvShowDao.kt
@@ -8,7 +8,6 @@ import com.red_velvet.flix.data.local.database.entity.AiringTodayTvShowEntity
 import com.red_velvet.flix.data.local.database.entity.OnTheAirTvShowEntity
 import com.red_velvet.flix.data.local.database.entity.PopularTvShowEntity
 import com.red_velvet.flix.data.local.database.entity.TopRatedTvShowEntity
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TvShowDao {
@@ -25,14 +24,14 @@ interface TvShowDao {
     suspend fun insertAiringTodayTvShows(airingTodayTvShowEntities: List<AiringTodayTvShowEntity>)
 
     @Query("SELECT * FROM PopularTvShowEntity")
-    suspend fun getPopularTvShow(): Flow<List<PopularTvShowEntity>>
+    fun getPopularTvShow(): List<PopularTvShowEntity>
 
     @Query("SELECT * FROM TopRatedTvShowEntity")
-    suspend fun getTopRatedTvShow(): Flow<List<TopRatedTvShowEntity>>
+    fun getTopRatedTvShow(): List<TopRatedTvShowEntity>
 
     @Query("SELECT * FROM OnTheAirTvShowEntity")
-    suspend fun getOnTheAirTvShow(): Flow<List<OnTheAirTvShowEntity>>
+    fun getOnTheAirTvShow(): List<OnTheAirTvShowEntity>
 
     @Query("SELECT * FROM AiringTodayTvShowEntity")
-    suspend fun getAiringTodayTvShow(): Flow<List<AiringTodayTvShowEntity>>
+    fun getAiringTodayTvShow(): List<AiringTodayTvShowEntity>
 }
