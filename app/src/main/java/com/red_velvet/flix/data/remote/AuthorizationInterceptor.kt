3f265b15f3a8dfc78616586b704670eebf272b11
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-06-06 22:05:07 +0300

    inject shared prefs inside auth interceptor

--- app/src/main/java/com/red_velvet/flix/data/remote/AuthorizationInterceptor.kt
+++ app/src/main/java/com/red_velvet/flix/data/remote/AuthorizationInterceptor.kt
@@ -1,18 +1,19 @@
 package com.red_velvet.flix.data.remote
 
 import com.red_velvet.flix.BuildConfig
+import com.red_velvet.flix.data.local.shared_prefs.SharedPrefs
+import kotlinx.coroutines.runBlocking
 import okhttp3.HttpUrl
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
+import javax.inject.Inject
 
-class AuthorizationInterceptor : Interceptor {
+class AuthorizationInterceptor @Inject constructor(
+    private val sharedPrefs: SharedPrefs
+) : Interceptor {
 
     private val apiKey: String = BuildConfig.API_KEY
-
-    // TODO: Jalal should give me a dependency, so that I can get these values from there
-    private val requestToken: String = ""
-    private val sessionId: String = ""
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
@@ -26,8 +27,8 @@ class AuthorizationInterceptor : Interceptor {
     private fun buildUrl(request: Request): HttpUrl {
         return request.url.newBuilder()
             .addQueryParameter(API_KEY, apiKey)
-            .addQueryParameter(REQUEST_TOKEN, requestToken)
-            .addQueryParameter(SESSION_ID, sessionId)
+            .addQueryParameter(REQUEST_TOKEN, runBlocking { sharedPrefs.getToken() })
+            .addQueryParameter(SESSION_ID, runBlocking { sharedPrefs.getSessionId() })
             .build()
     }
 
