f66c743588c38dcbadd21e5a803cb023d61f3066
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-06-06 17:40:35 +0300

    create user favorite and watch lists in user repository

--- app/src/main/java/com/red_velvet/flix/data/repository/UserRepository.kt
+++ app/src/main/java/com/red_velvet/flix/data/repository/UserRepository.kt
@@ -1,6 +1,8 @@
 package com.red_velvet.flix.data.repository
 
 import com.red_velvet.flix.data.remote.dtos.account.AccountDto
+import com.red_velvet.flix.domain.model.movie.Movie
+import com.red_velvet.flix.domain.model.series.TVShow
 
 interface UserRepository {
 
@@ -11,4 +13,36 @@ interface UserRepository {
     suspend fun getAccountDetails(): AccountDto?
 
     suspend fun getSessionId(): String?
+
+    suspend fun getFavoriteMovies(
+        language: String? = null,
+        page: Int? = null,
+        sortBy: String? = null,
+    ): List<Movie>
+
+    suspend fun getCachedFavoriteMovies(): List<Movie>
+
+    suspend fun getFavoriteSeries(
+        language: String? = null,
+        page: Int? = null,
+        sortBy: String? = null,
+    ): List<TVShow>
+
+    suspend fun getCachedFavoriteSeries(): List<TVShow>
+
+    suspend fun getWatchListMovies(
+        language: String? = null,
+        page: Int? = null,
+        sortBy: String? = null,
+    ): List<Movie>
+
+    suspend fun getCachedWatchListMovies(): List<Movie>
+
+    suspend fun getWatchListSeries(
+        language: String? = null,
+        page: Int? = null,
+        sortBy: String? = null,
+    ): List<TVShow>
+
+    suspend fun getCachedWatchListSeries(): List<TVShow>
 }
--- app/src/main/java/com/red_velvet/flix/data/repository/UserRepositoryImp.kt
+++ app/src/main/java/com/red_velvet/flix/data/repository/UserRepositoryImp.kt
@@ -1,14 +1,24 @@
 package com.red_velvet.flix.data.repository
 
+import com.red_velvet.flix.data.local.database.dao.UserDao
 import com.red_velvet.flix.data.local.sharedPrefs.SharedPrefs
 import com.red_velvet.flix.data.remote.MoviesService
 import com.red_velvet.flix.data.remote.dtos.account.AccountDto
 import com.red_velvet.flix.data.remote.dtos.auth.LoginRequest
+import com.red_velvet.flix.data.repository.mapper.movie.toFavoriteMovieEntityList
+import com.red_velvet.flix.data.repository.mapper.movie.toMovieEntityList
+import com.red_velvet.flix.data.repository.mapper.movie.toWatchedMovieEntityList
+import com.red_velvet.flix.data.repository.mapper.series.toFavoriteSeriesEntityList
+import com.red_velvet.flix.data.repository.mapper.series.toSeriesEntityList
+import com.red_velvet.flix.data.repository.mapper.series.toWatchedSeriesEntityList
+import com.red_velvet.flix.domain.model.movie.Movie
+import com.red_velvet.flix.domain.model.series.TVShow
 import com.red_velvet.flix.domain.utils.ExceptionHandler
 import javax.inject.Inject
 
 class UserRepositoryImp @Inject constructor(
     private val service: MoviesService,
+    private val userDao: UserDao,
     private val sharedPrefs: SharedPrefs,
     private val exceptionHandler: ExceptionHandler
 ) : UserRepository {
@@ -43,6 +53,74 @@ class UserRepositoryImp @Inject constructor(
 
     override suspend fun getSessionId(): String? {
         return sharedPrefs.getSessionId()
+    }
+
+    override suspend fun getFavoriteMovies(
+        language: String?,
+        page: Int?,
+        sortBy: String?
+    ): List<Movie> {
+        val response = service.getFavoriteMovies(language, page, sortBy)
+        if (response.isSuccessful) {
+            return response.body()?.items?.toMovieEntityList() ?: emptyList()
+        } else {
+            throw exceptionHandler.getException(response.code(), response.errorBody())
+        }
+    }
+
+    override suspend fun getCachedFavoriteMovies(): List<Movie> {
+        return userDao.getAllFavoriteMovies().toFavoriteMovieEntityList()
+    }
+
+    override suspend fun getFavoriteSeries(
+        language: String?,
+        page: Int?,
+        sortBy: String?
+    ): List<TVShow> {
+        val response = service.getFavoriteTvShows(language, page, sortBy)
+        if (response.isSuccessful) {
+            return response.body()?.items?.toSeriesEntityList() ?: emptyList()
+        } else {
+            throw exceptionHandler.getException(response.code(), response.errorBody())
+        }
+    }
+
+    override suspend fun getCachedFavoriteSeries(): List<TVShow> {
+        return userDao.getAllFavoriteSeries().toFavoriteSeriesEntityList()
+    }
+
+    override suspend fun getWatchListMovies(
+        language: String?,
+        page: Int?,
+        sortBy: String?
+    ): List<Movie> {
+        val response = service.getMoviesWatchlist(language, page, sortBy)
+        if (response.isSuccessful) {
+            return response.body()?.items?.toMovieEntityList() ?: emptyList()
+        } else {
+            throw exceptionHandler.getException(response.code(), response.errorBody())
+        }
+    }
+
+    override suspend fun getCachedWatchListMovies(): List<Movie> {
+        return userDao.getAllWatchedMovies().toWatchedMovieEntityList()
+    }
+
+    override suspend fun getWatchListSeries(
+        language: String?,
+        page: Int?,
+        sortBy: String?
+    ): List<TVShow> {
+        val response = service.getTvShowsWatchlist(language, page, sortBy)
+        if (response.isSuccessful) {
+            return response.body()?.items?.toSeriesEntityList() ?: emptyList()
+        } else {
+            throw exceptionHandler.getException(response.code(), response.errorBody())
+        }
+    }
+
+    override suspend fun getCachedWatchListSeries(): List<TVShow> {
+        return userDao.getAllWatchedSeries().toWatchedSeriesEntityList()
     }
 
     private suspend fun saveSessionId(sessionId: String) {
