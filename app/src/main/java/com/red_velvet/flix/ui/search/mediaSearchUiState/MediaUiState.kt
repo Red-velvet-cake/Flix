787f9ca9a5f3b4ac9f48fcc6ff7cac3c25ab04e0
mustafayassi <must3f3yassin@gmail.com>
2023-06-06 17:08:59 +0300

    add extension function to media ui state

--- app/src/main/java/com/red_velvet/flix/ui/search/mediaSearchUiState/MediaUiState.kt
+++ app/src/main/java/com/red_velvet/flix/ui/search/mediaSearchUiState/MediaUiState.kt
@@ -1,5 +1,8 @@
 package com.red_velvet.flix.ui.search.mediaSearchUiState
 
+import com.red_velvet.flix.BuildConfig
+import com.red_velvet.flix.domain.model.movie.Movie
+
 data class MediaUiState(
     val mediaID: Int,
     val mediaName: String,
@@ -8,3 +11,14 @@ data class MediaUiState(
     val mediaReleaseDate: String,
     val mediaVoteAverage: Double,
 )
+internal fun Movie.toUiState(): MediaUiState {
+    return MediaUiState(
+        mediaID = id,
+        mediaName = title,
+        mediaImage = BuildConfig.IMAGE_BASE_PATH,
+        mediaPopularity = popularity,
+        mediaReleaseDate = releaseDate,
+        mediaVoteAverage = voteAverage
+    )
+
+}
