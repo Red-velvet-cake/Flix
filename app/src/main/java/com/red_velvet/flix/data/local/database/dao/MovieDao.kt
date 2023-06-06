a3338dd86da94c17094f2f8f7c701b016aa556df
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-06-06 19:54:20 +0300

    add on conflict strategy to daos

--- app/src/main/java/com/red_velvet/flix/data/local/database/dao/MovieDao.kt
+++ app/src/main/java/com/red_velvet/flix/data/local/database/dao/MovieDao.kt
@@ -2,24 +2,26 @@ package com.red_velvet.flix.data.local.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.red_velvet.flix.data.local.database.entity.NowPlayingMovieEntity
 import com.red_velvet.flix.data.local.database.entity.PopularMovieEntity
 import com.red_velvet.flix.data.local.database.entity.TopRatedMovieEntity
 import com.red_velvet.flix.data.local.database.entity.UpcomingMovieEntity
 import kotlinx.coroutines.flow.Flow
+
 @Dao
 interface MovieDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertPopularMovies(popularMovieEntities: List<PopularMovieEntity>)
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTopRatedMovies(topRatedMovieEntities: List<TopRatedMovieEntity>)
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNowPlayingMovies(nowPlayingMovieEntities: List<NowPlayingMovieEntity>)
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertUpcomingMovies(upcomingMovieEntities: List<UpcomingMovieEntity>)
 
     @Query("SELECT * FROM PopularMovieEntity")
--- app/src/main/java/com/red_velvet/flix/data/local/database/dao/UserDataDao.kt
+++ app/src/main/java/com/red_velvet/flix/data/local/database/dao/UserDataDao.kt
@@ -7,7 +7,7 @@ import com.red_velvet.flix.data.local.database.entity.UserEntity
 
 @Dao
 interface UserDataDao {
-    @Insert
+    @Insert(onConflict = androidx.room.OnConflictStrategy.REPLACE)
     suspend fun insertUserData(userEntity: UserEntity)
 
     @Query("SELECT * FROM UserEntity")
