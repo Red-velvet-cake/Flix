c533d4d7f145991594186a97e13ae0c8f6669c4e
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-06 13:42:38 +0300

    show images

--- app/src/main/java/com/red_velvet/flix/ui/utils/BindingAdapters.kt
+++ app/src/main/java/com/red_velvet/flix/ui/utils/BindingAdapters.kt
@@ -1,10 +1,25 @@
 package com.red_velvet.flix.ui.utils
 
 import android.view.View
+import android.widget.ImageView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.red_velvet.flix.R
 
 @BindingAdapter("app:hideIfTrue")
 fun hideIfTrue(view: View, value: Boolean) {
     view.isVisible = !value
 }
+
+@BindingAdapter("app:posterImage")
+fun bindMovieImage(image: ImageView, imageURL: String?) {
+    imageURL?.let {
+        Glide.with(image.context)
+            .load(imageURL)
+            .placeholder(R.drawable.ic_loading)
+            .error(R.drawable.ic_media_background)
+            .into(image)
+    }
+}
+
