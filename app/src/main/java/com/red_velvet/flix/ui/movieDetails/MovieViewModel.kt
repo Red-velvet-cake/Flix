dd63c09c6dc5720b014037ba0e2f464a6288e2e1
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-07 16:57:27 +0300

    create view model

--- app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieViewModel.kt
+++ app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieViewModel.kt
@@ -1,4 +1,23 @@
 package com.red_velvet.flix.ui.movieDetails
 
-class MovieViewModel {
+import androidx.lifecycle.ViewModel
+import com.red_velvet.flix.domain.model.movie.MovieDetails
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+class MovieViewModel @Inject constructor(private val movieUiState: MovieUiState,private val movieDetails: MovieDetails): ViewModel() {
+    private val _uiState: MutableStateFlow<MovieUiState> = MutableStateFlow(movieUiState)
+    val uiState: StateFlow<MovieUiState> = _uiState
+    fun onFetchData() {
+        _uiState.update {
+            it.copy(
+                isLoading = false,
+            movieName = movieDetails.title,
+            imageUrl = movieDetails.imageUrl,
+            description = movieDetails.overview,
+            )
+        }
+    }
 }
