dd03d76c13f5786b6383747a2e1c78e2edbcdfd0
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-06-08 18:07:05 +0300

    handle exceptions in base repository

--- app/src/main/java/com/red_velvet/flix/data/repository/BaseRepository.kt
+++ app/src/main/java/com/red_velvet/flix/data/repository/BaseRepository.kt
@@ -1,16 +1,28 @@
 package com.red_velvet.flix.data.repository
 
-import com.red_velvet.flix.data.util.ExceptionHandler
+import com.red_velvet.flix.data.remote.APIErrorHandler
+import com.red_velvet.flix.domain.utils.FlixException
+import okhttp3.internal.http2.ConnectionShutdownException
 import retrofit2.Response
+import java.io.IOException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
-abstract class BaseRepository(private val exceptionHandler: ExceptionHandler) {
+abstract class BaseRepository(private val apiErrorHandler: APIErrorHandler) {
 
     suspend fun <T> wrapApiCall(call: suspend () -> Response<T>): T {
-        val response = call()
-        if (response.isSuccessful) {
-            return response.body()!!
-        } else {
-            throw exceptionHandler.getException(response.code(), response.errorBody())
+        try {
+            val response = call()
+            if (response.isSuccessful) return response.body()!!
+            throw apiErrorHandler.getFlixException(response.errorBody())
+        } catch (e: SocketTimeoutException) {
+            throw FlixException.TimeOut
+        } catch (e: UnknownHostException) {
+            throw FlixException.NoInternet
+        } catch (e: ConnectionShutdownException) {
+            throw FlixException.NoInternet
+        } catch (e: IOException) {
+            throw FlixException.NoInternet
         }
     }
 }
