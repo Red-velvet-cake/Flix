aa96326242200948842865d64cd6624ed610a87c
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-06-08 19:09:59 +0300

    create error ui state type checks

--- app/src/main/java/com/red_velvet/flix/ui/base/ErrorUiState.kt
+++ app/src/main/java/com/red_velvet/flix/ui/base/ErrorUiState.kt
@@ -8,4 +8,12 @@ sealed class ErrorUiState {
     object NoInternet : ErrorUiState()
     object TimeOut : ErrorUiState()
     object Unknown : ErrorUiState()
+
+    fun isUnAuthorized() = this is UnAuthorized
+    fun isServerError() = this is ServerError
+    fun isInvalidUsernameOrPassword() = this is InvalidUsernameOrPassword
+    fun isEmailNotVerified() = this is EmailNotVerified
+    fun isNoInternet() = this is NoInternet
+    fun isTimeOut() = this is TimeOut
+    fun isUnknown() = this is Unknown
 }
