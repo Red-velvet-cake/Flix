457e73f921d907a62fa0ac1665360efb6430f529
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-09 10:33:40 +0300

    change return type of invoke function of  GetMovieDetailsUseCase to movieDetails entity from movie entity

--- app/src/main/java/com/red_velvet/flix/domain/usecase/GetMovieDetailsUseCase.kt
+++ app/src/main/java/com/red_velvet/flix/domain/usecase/GetMovieDetailsUseCase.kt
@@ -1,5 +1,6 @@
 package com.red_velvet.flix.domain.usecase
 
+import com.red_velvet.flix.domain.entity.movie.MovieDetailsEntity
 import com.red_velvet.flix.domain.entity.movie.MovieEntity
 import com.red_velvet.flix.domain.repository.MovieRepository
 import javax.inject.Inject
@@ -7,7 +8,7 @@ import javax.inject.Inject
 class GetMovieDetailsUseCase @Inject constructor(
     private val movieRepository: MovieRepository
 ) {
-    suspend operator fun invoke(movieId: Int): MovieEntity {
+    suspend operator fun invoke(movieId: Int): MovieDetailsEntity {
         return movieRepository.getMovieDetails(movieId)
     }
 }
