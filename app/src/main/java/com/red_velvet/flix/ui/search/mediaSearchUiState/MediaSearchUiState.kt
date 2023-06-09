71482618d5490b77de6b64ba60eac2a9db6f7c02
mustafayassi <must3f3yassin@gmail.com>
2023-06-10 02:42:07 +0300

    edit error return typ in media ui state

--- app/src/main/java/com/red_velvet/flix/ui/search/mediaSearchUiState/MediaSearchUiState.kt
+++ app/src/main/java/com/red_velvet/flix/ui/search/mediaSearchUiState/MediaSearchUiState.kt
@@ -1,24 +1,24 @@
 package com.red_velvet.flix.ui.search.mediaSearchUiState
 
-import com.red_velvet.flix.BuildConfig
 import com.red_velvet.flix.domain.entity.movie.MovieEntity
 import com.red_velvet.flix.ui.base.BaseUiState
+import com.red_velvet.flix.ui.base.ErrorUiState
 import kotlin.Error
 
 
 data class MediaSearchUiState (
     val searchInput: String = "",
-    val searchTypes: SearchTypes = SearchTypes.MOVIE,
+    val searchTypes: SearchTypes = SearchTypes.ALL,
     val searchResult: List<MediaUiState> = emptyList(),
     val isLoading: Boolean = false,
     val isEmpty: Boolean = false,
-    val error: List<Error> = emptyList()
+    val error: ErrorUiState? = null
 ) : BaseUiState
 internal fun MovieEntity.toUiState(): MediaUiState {
     return MediaUiState(
         mediaID = id,
         mediaName = title,
-        mediaImage = BuildConfig.IMAGE_BASE_PATH,
+        mediaImage = imageUrl,
         mediaPopularity = popularity,
         mediaReleaseDate = releaseDate,
         mediaVoteAverage = voteAverage
