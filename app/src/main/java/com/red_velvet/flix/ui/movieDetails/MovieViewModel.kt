7c4a75bd6f82e55182f25fd95bf2a0245fa85db7
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-09 10:35:58 +0300

    implement sample of view model to fetch movie data

--- app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieViewModel.kt
+++ app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieViewModel.kt
@@ -1,23 +1,39 @@
 package com.red_velvet.flix.ui.movieDetails
 
-import androidx.lifecycle.ViewModel
-import com.red_velvet.flix.domain.model.movie.MovieDetails
+import androidx.lifecycle.viewModelScope
+import com.red_velvet.flix.domain.entity.movie.MovieDetailsEntity
+import com.red_velvet.flix.domain.usecase.GetMovieDetailsUseCase
+import com.red_velvet.flix.ui.base.BaseViewModel
+import com.red_velvet.flix.ui.base.ErrorUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MovieViewModel @Inject constructor(private val movieUiState: MovieUiState,private val movieDetails: MovieDetails): ViewModel() {
-    private val _uiState: MutableStateFlow<MovieUiState> = MutableStateFlow(movieUiState)
-    val uiState: StateFlow<MovieUiState> = _uiState
-    fun onFetchData() {
-        _uiState.update {
-            it.copy(
-                isLoading = false,
-            movieName = movieDetails.title,
-            imageUrl = movieDetails.imageUrl,
-            description = movieDetails.overview,
-            )
+class MovieViewMode @Inject constructor(private val getMovieDetailsUseCase: GetMovieDetailsUseCase) :
+    BaseViewModel<MovieUiState>() {
+    override val _state: MutableStateFlow<MovieUiState> = MutableStateFlow(MovieUiState())
+    override val state: StateFlow<MovieUiState> = _state
+
+    init {
+        onFetchMovieDetailsData()
+    }
+
+    private fun onFetchMovieDetailsData() {
+        viewModelScope.launch {
+            tryToExecute({ getMovieDetailsUseCase.invoke(MOVIE_ID) }, ::onSuccess, ::onError)
         }
+    }
+
+    private fun onSuccess(movieDetails: MovieDetailsEntity) {
+
+    }
+
+    private fun onError(error: ErrorUiState) {
+
+    }
+
+    companion object {
+        const val MOVIE_ID = 1
     }
 }
