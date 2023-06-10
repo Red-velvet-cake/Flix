7932800ac9e738233f6a0ff2996fcc0bbd68b7ff
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-10 11:26:12 +0300

    I forced to do this commit

---
+++ app/src/main/java/com/red_velvet/flix/domain/usecase/GetTopRatedMoviesUsecase.kt
@@ -0,0 +1,13 @@
+package com.red_velvet.flix.domain.usecase
+
+import com.red_velvet.flix.domain.entity.movie.MovieEntity
+import com.red_velvet.flix.domain.repository.MovieRepository
+import javax.inject.Inject
+
+class GetTopRatedMoviesUseCase @Inject constructor(
+    private val movieRepository: MovieRepository
+) {
+    suspend operator fun invoke(): List<MovieEntity> {
+        return movieRepository.getLocalTopRatedMovies()
+    }
+}
