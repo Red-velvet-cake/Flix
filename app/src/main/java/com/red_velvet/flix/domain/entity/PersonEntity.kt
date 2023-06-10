76fd58248336902fc690e85fddb8fe115d08c4df
mustafayassi <must3f3yassin@gmail.com>
2023-06-10 08:48:33 +0300

    add data body to person entity

--- app/src/main/java/com/red_velvet/flix/domain/entity/PersonEntity.kt
+++ app/src/main/java/com/red_velvet/flix/domain/entity/PersonEntity.kt
@@ -1,4 +1,11 @@
 package com.red_velvet.flix.domain.entity
 
-class PersonEntity {
-}
+import android.media.Image
+
+data class PersonEntity(
+    val name: String,
+    val id: Int,
+    val imageUrl: String,
+    val popularity: Double,
+    val birthday: String
+)
