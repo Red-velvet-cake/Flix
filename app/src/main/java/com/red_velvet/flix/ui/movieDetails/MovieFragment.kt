a66489890db77c677e8edf14e5727676297d30d2
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-14 20:16:42 +0300

    prepare fragment

--- app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieFragment.kt
+++ app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieFragment.kt
@@ -1,4 +1,14 @@
 package com.red_velvet.flix.ui.movieDetails
 
-class MovieFragment {
+import androidx.fragment.app.viewModels
+import com.red_velvet.flix.R
+import com.red_velvet.flix.databinding.FragmentMovieDetailsBinding
+import com.red_velvet.flix.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MovieFragment : BaseFragment<FragmentMovieDetailsBinding>(){
+    override val layoutIdFragment: Int
+        get() = R.layout.fragment_movie_details
+    override val viewModel: MovieDetailsViewModel by viewModels()
 }
