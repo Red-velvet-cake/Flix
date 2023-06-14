1209c1c586a0fca9ce2b37cf708f39f232f721b5
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-14 20:16:25 +0300

    make first change on state of ui state

--- app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieDetailsViewModel.kt
+++ app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieDetailsViewModel.kt
@@ -1,16 +1,19 @@
 package com.red_velvet.flix.ui.movieDetails
 
+import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.red_velvet.flix.domain.entity.movie.MovieDetailsEntity
 import com.red_velvet.flix.domain.usecase.GetMovieDetailsUseCase
 import com.red_velvet.flix.ui.base.BaseViewModel
 import com.red_velvet.flix.ui.base.ErrorUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-
-class MovieViewMode @Inject constructor(private val getMovieDetailsUseCase: GetMovieDetailsUseCase) :
+@HiltViewModel
+class MovieDetailsViewModel @Inject constructor(private val getMovieDetailsUseCase: GetMovieDetailsUseCase) :
     BaseViewModel<MovieUiState>() {
     override val _state: MutableStateFlow<MovieUiState> = MutableStateFlow(MovieUiState())
     override val state: StateFlow<MovieUiState> = _state
@@ -26,14 +29,14 @@ class MovieViewMode @Inject constructor(private val getMovieDetailsUseCase: GetM
     }
 
     private fun onSuccess(movieDetails: MovieDetailsEntity) {
-
+        _state.update { it.copy(isLoading = false) }
     }
 
     private fun onError(error: ErrorUiState) {
-
+        Log.i("jalalCheff",error.toString())
     }
 
     companion object {
-        const val MOVIE_ID = 1
+        const val MOVIE_ID = 385687
     }
 }
