10d6841028743cb8bc03a4e3c2d01e7b7f6e9f4f
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-15 00:41:37 +0300

    add image binding adapter

--- app/src/main/java/com/red_velvet/flix/ui/utils/BindingAdapters.kt
+++ app/src/main/java/com/red_velvet/flix/ui/utils/BindingAdapters.kt
@@ -1,10 +1,20 @@
 package com.red_velvet.flix.ui.utils
 
 import android.view.View
+import android.widget.ImageView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.red_velvet.flix.R
 
 @BindingAdapter("app:hideIfTrue")
 fun hideIfTrue(view: View, value: Boolean) {
     view.isVisible = !value
+}
+@BindingAdapter(value = ["app:imageUrl"])
+fun loadImage(view: ImageView, url: String?) {
+    Glide.with(view).load(url)
+        .fitCenter()
+        .centerCrop()
+        .into(view)
 }
