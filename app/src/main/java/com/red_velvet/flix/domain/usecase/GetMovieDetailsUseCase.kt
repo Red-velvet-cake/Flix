ab3b4c82752f7894c7e5fc0da6ec206302b10500
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-15 00:44:10 +0300

    inject GetFormattedMovieTimeUseCase into GetMovieDetailsUseCase

--- app/src/main/java/com/red_velvet/flix/domain/usecase/GetMovieDetailsUseCase.kt
+++ app/src/main/java/com/red_velvet/flix/domain/usecase/GetMovieDetailsUseCase.kt
@@ -1,14 +1,14 @@
 package com.red_velvet.flix.domain.usecase
 
 import com.red_velvet.flix.domain.entity.movie.MovieDetailsEntity
-import com.red_velvet.flix.domain.entity.movie.MovieEntity
 import com.red_velvet.flix.domain.repository.MovieRepository
 import javax.inject.Inject
 
 class GetMovieDetailsUseCase @Inject constructor(
-    private val movieRepository: MovieRepository
+    private val movieRepository: MovieRepository,
+    private val formattedMovieTime: GetFormattedMovieTimeUseCase
 ) {
     suspend operator fun invoke(movieId: Int): MovieDetailsEntity {
-        return movieRepository.getMovieDetails(movieId)
+        return movieRepository.getMovieDetails(movieId).copy(runtime = formattedMovieTime.invoke(movieId))
     }
 }
