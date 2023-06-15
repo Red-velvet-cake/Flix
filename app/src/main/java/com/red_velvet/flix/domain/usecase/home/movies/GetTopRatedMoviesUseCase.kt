c60cab62271ba5ff53d06a60d6d837f36e55767b
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-15 14:26:55 +0300

    cache top rated movies

--- app/src/main/java/com/red_velvet/flix/domain/usecase/home/movies/GetTopRatedMoviesUseCase.kt
+++ app/src/main/java/com/red_velvet/flix/domain/usecase/home/movies/GetTopRatedMoviesUseCase.kt
@@ -2,12 +2,26 @@ package com.red_velvet.flix.domain.usecase.home.movies
 
 import com.red_velvet.flix.domain.entity.movie.MovieEntity
 import com.red_velvet.flix.domain.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEmpty
 import javax.inject.Inject
+
 
 class GetTopRatedMoviesUseCase @Inject constructor(
     private val movieRepository: MovieRepository
 ) {
-    suspend operator fun invoke(): List<MovieEntity> {
-        return movieRepository.getLocalTopRatedMovies()
+
+    suspend operator fun invoke(): Flow<List<MovieEntity>> {
+        return movieRepository.getLocalTopRatedMovies().onEmpty {
+            saveTopRatedMoviesLocal()
+        }
+    }
+
+    private suspend fun saveTopRatedMoviesLocal() {
+        movieRepository.cacheTopRatedMovies(getTopRatedMovies())
+    }
+
+    private suspend fun getTopRatedMovies(): List<MovieEntity> {
+        return movieRepository.getTopRatedMovies()
     }
 }
