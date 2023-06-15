9b5454d19c53cd936544b6683685cceac1d8cd2c
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-15 14:58:00 +0300

    cache popular series

--- app/src/main/java/com/red_velvet/flix/domain/usecase/home/tvshows/GetPopularSeriesUseCase.kt
+++ app/src/main/java/com/red_velvet/flix/domain/usecase/home/tvshows/GetPopularSeriesUseCase.kt
@@ -2,10 +2,25 @@ package com.red_velvet.flix.domain.usecase.home.tvshows
 
 import com.red_velvet.flix.domain.entity.series.SeriesEntity
 import com.red_velvet.flix.domain.repository.SeriesRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEmpty
 import javax.inject.Inject
 
-class GetPopularSeriesUseCase @Inject constructor(private val seriesRepository: SeriesRepository) {
-    suspend operator fun invoke(): List<SeriesEntity> {
-        return seriesRepository.getLocalPopularSeries()
+class GetPopularSeriesUseCase @Inject constructor(
+    private val seriesRepository: SeriesRepository
+) {
+
+    suspend operator fun invoke(): Flow<List<SeriesEntity>> {
+        return seriesRepository.getLocalPopularSeries().onEmpty {
+            savePopularSeriesLocal()
+        }
+    }
+
+    private suspend fun savePopularSeriesLocal() {
+        seriesRepository.cachePopularSeries(getPopularSeries())
+    }
+
+    private suspend fun getPopularSeries(): List<SeriesEntity> {
+        return seriesRepository.getPopularSeries()
     }
 }
