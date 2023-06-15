1bf12ea56506d2a444e25632a3336cdc3dd8adda
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-15 23:06:01 +0300

    add function to BindingAdapters.kt to implement review recycler

--- app/src/main/java/com/red_velvet/flix/ui/utils/BindingAdapters.kt
+++ app/src/main/java/com/red_velvet/flix/ui/utils/BindingAdapters.kt
@@ -10,6 +10,7 @@ import com.red_velvet.flix.R
 import com.red_velvet.flix.domain.entity.movie.MovieEntity
 import com.red_velvet.flix.ui.movieDetails.MovieDetailsAdapter
 import com.red_velvet.flix.ui.movieDetails.MovieUiState
+import com.red_velvet.flix.ui.movieDetails.ReviewMovieDetailsAdapter
 
 @BindingAdapter("app:hideIfTrue")
 fun hideIfTrue(view: View, value: Boolean) {
@@ -23,6 +24,10 @@ fun loadImage(view: ImageView, url: String?) {
         .into(view)
 }
 @BindingAdapter(value = ["app:items"])
-fun setRecyclerViewItems(view: RecyclerView , items : List<MovieUiState.MovieList>){
-    (view.adapter as MovieDetailsAdapter).setItems(items)
+fun setRecyclerViewItems(view: RecyclerView , items : List<MovieUiState.MovieList>?){
+    items?.let { (view.adapter as MovieDetailsAdapter).setItems(items)}
+}
+@BindingAdapter(value = ["app:itemsReview"])
+fun setRecyclerViewItemsReview(view: RecyclerView , items : List<MovieUiState.MovieReview>?){
+    items?.let { (view.adapter as ReviewMovieDetailsAdapter).setItems(items)}
 }
