4f1ed42b02535496e52431b0a7d86bf78f56ab13
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-15 18:29:02 +0300

    implement MovieDetailsAdapter

---
+++ app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieDetailsAdapter.kt
@@ -0,0 +1,12 @@
+package com.red_velvet.flix.ui.movieDetails
+
+import com.red_velvet.flix.R
+import com.red_velvet.flix.ui.base.BaseAdapter
+import com.red_velvet.flix.ui.base.BaseInteractionListener
+
+
+class MovieDetailsAdapter(private val movieDetailsItems:MutableList<MovieUiState.MovieList>,
+private val listener: BaseInteractionListener):
+    BaseAdapter<MovieUiState.MovieList>(movieDetailsItems,listener) {
+    override val layoutId: Int = R.layout.item_movies_details
+}
