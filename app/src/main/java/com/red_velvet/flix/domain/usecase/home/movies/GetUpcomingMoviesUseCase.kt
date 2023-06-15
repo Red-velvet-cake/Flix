850a9bb8ea29743ddffdae3ad1b879cca1208768
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-15 14:28:42 +0300

    cache upcoming movies

--- app/src/main/java/com/red_velvet/flix/domain/usecase/home/movies/GetUpcomingMoviesUseCase.kt
+++ app/src/main/java/com/red_velvet/flix/domain/usecase/home/movies/GetUpcomingMoviesUseCase.kt
@@ -2,12 +2,25 @@ package com.red_velvet.flix.domain.usecase.home.movies
 
 import com.red_velvet.flix.domain.entity.movie.MovieEntity
 import com.red_velvet.flix.domain.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEmpty
 import javax.inject.Inject
 
 class GetUpcomingMoviesUseCase @Inject constructor(
     private val movieRepository: MovieRepository
 ) {
-    suspend operator fun invoke(): List<MovieEntity> {
-        return movieRepository.getLocalUpcomingMovies()
+
+    suspend operator fun invoke(): Flow<List<MovieEntity>> {
+        return movieRepository.getLocalUpcomingMovies().onEmpty {
+            saveUpcomingMoviesLocal()
+        }
+    }
+
+    private suspend fun saveUpcomingMoviesLocal() {
+        movieRepository.cacheUpcomingMovies(getUpcomingMovies())
+    }
+
+    private suspend fun getUpcomingMovies(): List<MovieEntity> {
+        return movieRepository.getUpcomingMovies()
     }
 }
