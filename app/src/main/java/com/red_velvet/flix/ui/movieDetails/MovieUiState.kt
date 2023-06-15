3f99c3f328094e83e52bf4ea62e5d3852e0b018e
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-15 18:31:31 +0300

    add mapping function from movie entity to movie list

--- app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieUiState.kt
+++ app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieUiState.kt
@@ -1,11 +1,12 @@
 package com.red_velvet.flix.ui.movieDetails
 
 import com.red_velvet.flix.domain.entity.movie.MovieDetailsEntity
+import com.red_velvet.flix.domain.entity.movie.MovieEntity
 import com.red_velvet.flix.ui.base.BaseUiState
 
 data class MovieUiState(
-    val isLoading : Boolean = true,
-    val errors : List<String> = emptyList(),
+    val isLoading: Boolean = true,
+    val errors: List<String> = emptyList(),
     val isFavorite: Boolean = false,
     val isSaved: Boolean = false,
     val goBack: Boolean = false,
@@ -15,7 +16,7 @@ data class MovieUiState(
     val movieCategories: List<String> = emptyList(),
     val movieTime: String = "",
     val movieLanguage: String = "",
-    val movieRating: Double = 0.0,
+    val movieRating: Int = 0,
     val rateTheMovie: Boolean = false,
     val description: String = "",
     val seeAllCast: Boolean = false,
@@ -25,25 +26,28 @@ data class MovieUiState(
     val productionCountries: String = "",
     val keyWords: List<String> = emptyList(),
     val seeAllSimilarMovies: Boolean = false,
-    val similarMovies: List<SimilarMovie> = emptyList(),
+    val similarMovies: List<MovieList> = emptyList(),
     val seeAllImagesBelongToMovie: Boolean = false,
     val imageBelongToMovie: List<String> = emptyList(),
     val latest: String = "",
     val seeAllReviews: Boolean = false,
     val review: List<MovieReview> = emptyList(),
     val seeAllRecommendations: Boolean = false,
-    val movieRecommendations: List<MovieRecommendations> = emptyList(),
-): BaseUiState
+    val movieRecommendations: List<MovieList> = emptyList(),
+) : BaseUiState
+{
+    data class MovieList(
+        val movieImageUrl: String,
+        val movieName: String,
+    )
+}
 
 data class TopCast(
     val actorImageUrl: String,
     val actorName: String,
 )
 
-data class SimilarMovie(
-    val movieImageUrl: String,
-    val movieName: String,
-)
+
 
 data class MovieReview(
     val rating: Double,
@@ -51,12 +55,23 @@ data class MovieReview(
     val movieReview: String,
 )
 
-data class MovieRecommendations(
+/*data class MovieRecommendations(
     val recommendedMovieName: String,
     val recommendedMovieImageUrl: String,
-)
-fun MovieDetailsEntity.toUiState(): MovieUiState{
-return MovieUiState(
+)*/
+fun MovieDetailsEntity.toUiState(): MovieUiState {
+    return MovieUiState(
         movieName = title
+    )
+}
+
+fun List<MovieEntity>.toListOfMovies(): List<MovieUiState.MovieList> {
+    return map { it.toMoviesUiState() }
+}
+
+fun MovieEntity.toMoviesUiState(): MovieUiState.MovieList {
+    return MovieUiState.MovieList(
+        movieName = title,
+        movieImageUrl = imageUrl
     )
 }
