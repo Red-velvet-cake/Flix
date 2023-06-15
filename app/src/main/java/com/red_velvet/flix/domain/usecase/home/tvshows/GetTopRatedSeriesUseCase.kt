3918414c376975ce937e8e43ff04e006b5b0a78a
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-15 14:58:40 +0300

    cache top rated series

--- app/src/main/java/com/red_velvet/flix/domain/usecase/home/tvshows/GetTopRatedSeriesUseCase.kt
+++ app/src/main/java/com/red_velvet/flix/domain/usecase/home/tvshows/GetTopRatedSeriesUseCase.kt
@@ -2,10 +2,25 @@ package com.red_velvet.flix.domain.usecase.home.tvshows
 
 import com.red_velvet.flix.domain.entity.series.SeriesEntity
 import com.red_velvet.flix.domain.repository.SeriesRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEmpty
 import javax.inject.Inject
 
-class GetTopRatedSeriesUseCase @Inject constructor(private val seriesRepository: SeriesRepository) {
-    suspend operator fun invoke(): List<SeriesEntity> {
-        return seriesRepository.getLocalTopRatedSeries()
+class GetTopRatedSeriesUseCase @Inject constructor(
+    private val seriesRepository: SeriesRepository
+) {
+
+    suspend operator fun invoke(): Flow<List<SeriesEntity>> {
+        return seriesRepository.getLocalTopRatedSeries().onEmpty {
+            saveTopRatedSeriesLocal()
+        }
+    }
+
+    private suspend fun saveTopRatedSeriesLocal() {
+        seriesRepository.cacheTopRatedSeries(getTopRatedSeries())
+    }
+
+    private suspend fun getTopRatedSeries(): List<SeriesEntity> {
+        return seriesRepository.getTopRatedSeries()
     }
 }
