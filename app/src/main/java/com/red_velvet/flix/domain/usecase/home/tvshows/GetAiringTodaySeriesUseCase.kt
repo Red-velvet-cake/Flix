c2e3b09d1e9b603ee4cc261bd844a27ba58350a4
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-15 14:55:57 +0300

    cache airing today series

--- app/src/main/java/com/red_velvet/flix/domain/usecase/home/tvshows/GetAiringTodaySeriesUseCase.kt
+++ app/src/main/java/com/red_velvet/flix/domain/usecase/home/tvshows/GetAiringTodaySeriesUseCase.kt
@@ -2,10 +2,25 @@ package com.red_velvet.flix.domain.usecase.home.tvshows
 
 import com.red_velvet.flix.domain.entity.series.SeriesEntity
 import com.red_velvet.flix.domain.repository.SeriesRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onEmpty
 import javax.inject.Inject
 
-class GetAiringTodaySeriesUseCase @Inject constructor(private val seriesRepository: SeriesRepository) {
-    suspend operator fun invoke(): List<SeriesEntity> {
-        return seriesRepository.getLocalAiringTodaySeries()
+class GetAiringTodaySeriesUseCase @Inject constructor(
+    private val seriesRepository: SeriesRepository
+) {
+
+    suspend operator fun invoke(): Flow<List<SeriesEntity>> {
+        return seriesRepository.getLocalAiringTodaySeries().onEmpty {
+            saveAiringTodaySeriesLocal()
+        }
+    }
+
+    private suspend fun saveAiringTodaySeriesLocal() {
+        seriesRepository.cacheAiringTodaySeries(getAiringTodaySeries())
+    }
+
+    private suspend fun getAiringTodaySeries(): List<SeriesEntity> {
+        return seriesRepository.getAiringTodaySeries()
     }
 }
