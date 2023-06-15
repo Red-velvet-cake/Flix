e66b031053a069e7d073aefd9b1f90f9bf39195b
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-15 18:33:27 +0300

    call adapter and assign list from fragment

--- app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieFragment.kt
+++ app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieFragment.kt
@@ -1,5 +1,7 @@
 package com.red_velvet.flix.ui.movieDetails
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.red_velvet.flix.R
 import com.red_velvet.flix.databinding.FragmentMovieDetailsBinding
@@ -11,4 +13,10 @@ class MovieFragment : BaseFragment<FragmentMovieDetailsBinding>(){
     override val layoutIdFragment: Int
         get() = R.layout.fragment_movie_details
     override val viewModel: MovieDetailsViewModel by viewModels()
+    lateinit var adapter: MovieDetailsAdapter
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adapter = MovieDetailsAdapter(mutableListOf(),viewModel)
+        binding.RecommendationsScroll.adapter = adapter
+    }
 }
