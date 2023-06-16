f13cb7162ec74ffbd689ff44b8f01d4d06dd8e87
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-17 01:59:10 +0300

    add adapters of movie recycler views

--- app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieFragment.kt
+++ app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieFragment.kt
@@ -3,29 +3,33 @@ package com.red_velvet.flix.ui.movieDetails
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.red_velvet.flix.R
 import com.red_velvet.flix.databinding.FragmentMovieDetailsBinding
 import com.red_velvet.flix.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MovieFragment : BaseFragment<FragmentMovieDetailsBinding>(){
     override val layoutIdFragment: Int
         get() = R.layout.fragment_movie_details
     override val viewModel: MovieDetailsViewModel by viewModels()
-    lateinit var adapter: MovieDetailsAdapter
+    private lateinit var adapter: MovieDetailsAdapter
     private lateinit var similarMovieAdapter: MovieDetailsAdapter
-    lateinit var movieDetailsReviewAdapter: ReviewMovieDetailsAdapter
+    private lateinit var movieDetailsReviewAdapter: ReviewMovieDetailsAdapter
+    private lateinit var movieDetailsImagesAdapter: MovieImagesAdapter
+    private lateinit var movieTopCastAdapter: MovieTopCastAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         adapter = MovieDetailsAdapter(mutableListOf(),viewModel)
         similarMovieAdapter = MovieDetailsAdapter(mutableListOf(),viewModel)
         movieDetailsReviewAdapter = ReviewMovieDetailsAdapter(mutableListOf(),viewModel)
+        movieDetailsImagesAdapter = MovieImagesAdapter(mutableListOf(),viewModel)
+        movieTopCastAdapter = MovieTopCastAdapter(mutableListOf(),viewModel)
         binding.RecommendationsScroll.adapter = adapter
         binding.similarMoviesScroll.adapter = similarMovieAdapter
         binding.reviewsScroll.adapter = movieDetailsReviewAdapter
+        binding.ImagesBelongToMovieScroll.adapter = movieDetailsImagesAdapter
+        binding.topCastScrollView.adapter = movieTopCastAdapter
         }
     }
