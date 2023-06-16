82c14c21e39cd4a069be6f64e8b12b7fd7716aec
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-17 01:45:29 +0300

    complete caching top rated series usecase

--- app/src/main/java/com/red_velvet/flix/domain/usecase/home/tvshows/GetTopRatedSeriesUseCase.kt
+++ app/src/main/java/com/red_velvet/flix/domain/usecase/home/tvshows/GetTopRatedSeriesUseCase.kt
@@ -2,25 +2,29 @@ package com.red_velvet.flix.domain.usecase.home.tvshows
 
 import com.red_velvet.flix.domain.entity.series.SeriesEntity
 import com.red_velvet.flix.domain.repository.SeriesRepository
+import com.red_velvet.flix.domain.usecase.caching.ShouldCacheApiResponseUseCase
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.onEmpty
 import javax.inject.Inject
 
 class GetTopRatedSeriesUseCase @Inject constructor(
-    private val seriesRepository: SeriesRepository
+    private val seriesRepository: SeriesRepository,
+    private val shouldCacheApiResponseUseCase: ShouldCacheApiResponseUseCase
 ) {
 
     suspend operator fun invoke(): Flow<List<SeriesEntity>> {
-        return seriesRepository.getLocalTopRatedSeries().onEmpty {
-            saveTopRatedSeriesLocal()
+        if (shouldCacheApiResponseUseCase("top_rated_series")) {
+            refreshLocalTopRatedSeries()
         }
-    }
-
-    private suspend fun saveTopRatedSeriesLocal() {
-        seriesRepository.cacheTopRatedSeries(getTopRatedSeries())
+        return seriesRepository.getLocalTopRatedSeries()
     }
 
     private suspend fun getTopRatedSeries(): List<SeriesEntity> {
         return seriesRepository.getTopRatedSeries()
     }
+
+    private suspend fun refreshLocalTopRatedSeries() {
+        val topRatedSeries = getTopRatedSeries()
+        seriesRepository.cacheTopRatedSeries(topRatedSeries)
+    }
+
 }
