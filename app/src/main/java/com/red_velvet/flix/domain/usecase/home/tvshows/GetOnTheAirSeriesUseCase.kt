73036a6cee883b409d400f95987137c1021d1c98
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-06-17 01:41:25 +0300

    complete caching on the air series usecase

--- app/src/main/java/com/red_velvet/flix/domain/usecase/home/tvshows/GetOnTheAirSeriesUseCase.kt
+++ app/src/main/java/com/red_velvet/flix/domain/usecase/home/tvshows/GetOnTheAirSeriesUseCase.kt
@@ -2,26 +2,30 @@ package com.red_velvet.flix.domain.usecase.home.tvshows
 
 import com.red_velvet.flix.domain.entity.series.SeriesEntity
 import com.red_velvet.flix.domain.repository.SeriesRepository
+import com.red_velvet.flix.domain.usecase.caching.ShouldCacheApiResponseUseCase
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.onEmpty
 import javax.inject.Inject
 
 
 class GetOnTheAirSeriesUseCase @Inject constructor(
-    private val seriesRepository: SeriesRepository
+    private val seriesRepository: SeriesRepository,
+    private val shouldCacheApiResponseUseCase: ShouldCacheApiResponseUseCase
 ) {
 
     suspend operator fun invoke(): Flow<List<SeriesEntity>> {
-        return seriesRepository.getLocalOnTheAirSeries().onEmpty {
-            saveOnTheAirSeriesLocal()
+        if (shouldCacheApiResponseUseCase("on_the_air_series")) {
+            refreshLocalOnTheAirSeries()
         }
-    }
-
-    private suspend fun saveOnTheAirSeriesLocal() {
-        seriesRepository.cacheOnTheAirSeries(getOnTheAirSeries())
+        return seriesRepository.getLocalOnTheAirSeries()
     }
 
     private suspend fun getOnTheAirSeries(): List<SeriesEntity> {
         return seriesRepository.getOnTheAirSeries()
     }
+
+    private suspend fun refreshLocalOnTheAirSeries() {
+        val onTheAirSeries = getOnTheAirSeries()
+        seriesRepository.cacheOnTheAirSeries(onTheAirSeries)
+    }
+
 }
