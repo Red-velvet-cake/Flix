46fa000e2d16d3650c914bb70b9516c129e98159
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-17 01:48:46 +0300

    add functions to binding adapter

--- app/src/main/java/com/red_velvet/flix/ui/utils/BindingAdapters.kt
+++ app/src/main/java/com/red_velvet/flix/ui/utils/BindingAdapters.kt
@@ -6,9 +6,9 @@ import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.red_velvet.flix.R
-import com.red_velvet.flix.domain.entity.movie.MovieEntity
+import com.red_velvet.flix.ui.movieDetails.MovieImagesAdapter
 import com.red_velvet.flix.ui.movieDetails.MovieDetailsAdapter
+import com.red_velvet.flix.ui.movieDetails.MovieTopCastAdapter
 import com.red_velvet.flix.ui.movieDetails.MovieUiState
 import com.red_velvet.flix.ui.movieDetails.ReviewMovieDetailsAdapter
 
@@ -30,4 +30,12 @@ fun setRecyclerViewItems(view: RecyclerView , items : List<MovieUiState.MovieLis
 @BindingAdapter(value = ["app:itemsReview"])
 fun setRecyclerViewItemsReview(view: RecyclerView , items : List<MovieUiState.MovieReview>?){
     items?.let { (view.adapter as ReviewMovieDetailsAdapter).setItems(items)}
+}
+@BindingAdapter(value = ["app:itemsImagesBelongToMovie"])
+fun setRecyclerViewItemsImages(view: RecyclerView , items : List<MovieUiState.ImageBelongToMovie>?){
+    items?.let { (view.adapter as MovieImagesAdapter).setItems(items)}
+}
+@BindingAdapter(value = ["app:itemsTopCast"])
+fun setRecyclerViewItemsTopCast(view: RecyclerView , items : List<MovieUiState.TopCast>?){
+    items?.let { (view.adapter as MovieTopCastAdapter).setItems(items)}
 }
