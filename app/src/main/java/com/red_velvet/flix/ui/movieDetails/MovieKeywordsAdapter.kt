31994d6768d69f9164b2f48bfa65052ac0ffab7c
jalal cheff <jlalalmyahy571@gmail.com>
2023-06-17 04:25:31 +0300

    implement MovieKeywordsAdapter

---
+++ app/src/main/java/com/red_velvet/flix/ui/movieDetails/MovieKeywordsAdapter.kt
@@ -0,0 +1,12 @@
+package com.red_velvet.flix.ui.movieDetails
+
+import com.red_velvet.flix.R
+import com.red_velvet.flix.ui.base.BaseAdapter
+import com.red_velvet.flix.ui.base.BaseInteractionListener
+
+class MovieKeywordsAdapter(
+    keywords:List<MovieUiState.MovieKeywords>,
+    listener: BaseInteractionListener
+):BaseAdapter<MovieUiState.MovieKeywords>(keywords,listener) {
+    override val layoutId: Int = R.layout.item_keywords_movie_details
+}
